bec3ce52632d80505fc0d59269ebe37d728d585a
tonomony <tonomony@icloud.com>
2018-11-15 14:40:47 +0100

    renamed package

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AppService } from '../core/services/app.service';
-import { LoadingService } from 'tonomony-ng-core-lib';
+import { LoadingService } from '@tonomony/ng-core-lib';
 
 @Component({
   selector: 'app-home',
